229d246af4a6f12c3f3aeb676842afc3e52b38dc
lou <louamaya@me.com>
2021-02-09 05:57:05 -0800

    add js

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#register').click(() => {
+        $('#usermodal').modal('show')
+    });
+
+    $('#register_login').click(() => {
+        $('#usermodal').modal('show')
+    });
+
+});
---
+++ static/js/registration.js
@@ -0,0 +1,14 @@
+const URL = "http://127.0.0.1:5000"
+
+
+async function verifyEmail(email) {
+    let res = await axios.post(`${URL}/email/${email}`)
+    if (res.data.length > 0) {
+        $('#email').val("")
+        $('#emailtakenmodal').modal('show')
+    }
+}
+
+$('#email').change(() => {
+    verifyEmail($('#email').val())
+})
