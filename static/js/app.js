776d00742026bbd83bac4008970814fc4d823626
lou <louamaya@me.com>
2021-02-10 05:27:27 -0800

    handle modal clicks for login registration comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    $('#login').click(() => {
+        $('#loginmodal').modal('show')
+    })
     $('#register').click(() => {
         $('#usermodal').modal('show')
     });
@@ -7,4 +10,13 @@ $(document).ready(function () {
         $('#usermodal').modal('show')
     });
 
+    $('#sendMessage').click(() => {
+        if ($('#session').length) {
+            console.log("You are in session")
+        } else {
+            console.log("You are NOT in session")
+        }
+    })
+
+    
 });
