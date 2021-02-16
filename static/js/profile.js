7ae088424773ce4391e84faadd26ca1ea35cbfcf
lou <louamaya@me.com>
2021-02-16 06:31:55 -0800

    add profile js to populate cities to select element and manage other js on profile page

---
+++ static/js/profile.js
@@ -0,0 +1,25 @@
+const URL = "http://127.0.0.1:5000"
+
+async function loadCities() {
+    try {
+        const response = await axios.post(`${URL}/cities`)
+        console.log(response)
+        cities = response.data.cities
+        cities.forEach((city) => {
+            $('#cities').append(`<option value=${city.id}>${city.city}</option>`)
+        })
+
+        services = response.data.services
+        services.forEach((service) => {
+            s = service.toLowerCase()
+            $(`#${s}`).attr('checked', 'True')
+        }
+        )
+        $('#cities').val(response.data.user.id)
+        
+    } catch (e) {
+        console.log(e)
+    }
+    
+}
+loadCities()
