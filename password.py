48ee30d566d3a1c19c39952a5a47eca4685861dc
lou <louamaya@me.com>
2021-02-22 03:59:44 -0800

    created to handle password changes and password requirement validation

---
+++ password.py
@@ -0,0 +1,35 @@
+import re
+from models import db, User
+from decouple import config
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
+ACCESS_KEY = config('KEY')
+API_URL = 'http://apilayer.net/api/check'
+valid_data = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+
+
+class Password():
+
+    def __init__(self, form):
+        """Instantiate Password Class"""
+
+        self.password = form.password.data
+
+    def valid_password(self):
+        """Handles password validation if it meets criteria"""
+
+        # Compile Regex
+        valid = re.compile(valid_data)
+        # Search password for invalid input
+        is_valid = re.search(valid, self.password)
+        if is_valid:
+            return True
+        return False
+
+    def save_password(self, user, new_pass):
+        hashed = bcrypt.generate_password_hash(new_pass)
+        # turn byte string to normal (unicode utf8) string
+        user.password = hashed.decode("utf8")
+        db.session.add(user)
+        db.session.commit()
