450e8f97e887867d6406faf431ceae56d0b7626a
lou <louamaya@me.com>
2021-02-22 04:07:11 -0800

    fix errors, modify class for facebook,mobile, title and description variables

--- registration.py
+++ registration.py
@@ -1,4 +1,3 @@
-import re
 import requests
 from models import db, User, User_Type, Type, User_Service
 from decouple import config
@@ -6,8 +5,7 @@ from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 ACCESS_KEY = config('KEY')
-API_URL = 'http://apilayer.net/api/check'
-valid_data = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+API_URL = config('API_URL')
 
 
 class Registration():
@@ -28,12 +26,11 @@ class Registration():
         api_data = requests.get(
             f"{API_URL}?access_key={ACCESS_KEY}&email={self.email}")
         data = api_data.json()
-        print(data)
         if data['mx_found']:
             return True
         return False
 
-    def register_user(self, facebook, mobile):
+    def register_user(self, facebook="", mobile="", title="", description=""):
         """Handles user registration"""
 
         hashed = bcrypt.generate_password_hash(self.password)
@@ -49,6 +46,8 @@ class Registration():
             facebook=facebook,
             mobile=mobile,
             city_id=self.city_id,
+            title=title,
+            description=description
         )
         db.session.add(user)
         db.session.commit()
@@ -73,100 +72,34 @@ class Registration():
         db.session.add(user_type)
         db.session.commit()
 
-    def Add_Services(self, user_id, carpenter, painter, plumber, electrician):
-        """Add services to user"""
-        if carpenter:
-            carpenter = User_Service(
-                user_id=user_id,
-                service_id=1
-            )
-            db.session.add(carpenter)
+    # def Add_Services(self, user_id, carpenter, painter, plumber, electrician):
+    #     """Add services to user"""
+    #     if carpenter:
+    #         carpenter = User_Service(
+    #             user_id=user_id,
+    #             service_id=1
+    #         )
+    #         db.session.add(carpenter)
 
-        if painter:
-            painter = User_Service(
-                user_id=user_id,
-                service_id=2
-            )
-            db.session.add(painter)
+    #     if painter:
+    #         painter = User_Service(
+    #             user_id=user_id,
+    #             service_id=2
+    #         )
+    #         db.session.add(painter)
 
-        if plumber:
-            plumber = User_Service(
-                user_id=user_id,
-                service_id=3
-            )
-            db.session.add(plumber)
+    #     if plumber:
+    #         plumber = User_Service(
+    #             user_id=user_id,
+    #             service_id=3
+    #         )
+    #         db.session.add(plumber)
 
-        if electrician:
-            electrician = User_Service(
-                user_id=user_id,
-                service_id=4
-            )
-            db.session.add(electrician)
+    #     if electrician:
+    #         electrician = User_Service(
+    #             user_id=user_id,
+    #             service_id=4
+    #         )
+    #         db.session.add(electrician)
 
-        db.session.commit()
-
-
-class Password():
-
-    def __init__(self, form):
-        """Instantiate Password Class"""
-
-        self.password = form.password.data
-
-    def valid_password(self):
-        """Handles password validation if it meets criteria"""
-
-        # Compile Regex
-        valid = re.compile(valid_data)
-        # Search password for invalid input
-        is_valid = re.search(valid, self.password)
-        if is_valid:
-            return True
-        return False
-
-    def save_password(self, user, new_pass):
-        hashed = bcrypt.generate_password_hash(new_pass)
-        # turn byte string to normal (unicode utf8) string
-        user.password = hashed.decode("utf8")
-        db.session.add(user)
-        db.session.commit()
-
-
-class ServiceType():
-    def __init__(self, carpenter, painter, electrician, plumber):
-        """Instantiate Service Type"""
-        self.carpenter = carpenter
-        self.painter = painter
-        self.electrician = electrician
-        self.plumber = plumber
-
-    def UserService(self, user_id):
-        if self.carpenter:
-            carpenter = User_Service(
-                user_id=user_id,
-                service_id=1
-            )
-            db.session.add(carpenter)
-
-        if self.painter:
-            painter = User_Service(
-                user_id=user_id,
-                service_id=2
-            )
-            db.session.add(painter)
-
-        if self.plumber:
-            plumber = User_Service(
-                user_id=user_id,
-                service_id=3
-            )
-            db.session.add(plumber)
-
-        if self.electrician:
-            electrician = User_Service(
-                user_id=user_id,
-                service_id=4
-            )
-            db.session.add(electrician)
-
-        db.session.commit()
+    #     db.session.commit()
