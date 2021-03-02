4847e97e68950e962288b4fe5f3ceffbbb6e95b1
lou <louamaya@me.com>
2021-03-02 05:00:37 -0800

    change seed

--- seed.py
+++ seed.py
@@ -2,7 +2,6 @@
 from csv import DictReader
 from models import db, User, City, Service, User_Service, Type, User_Type, Job, User_Job, Comment, Message
 
-db.drop_all()
 db.create_all()
 
 # Create Types of User
