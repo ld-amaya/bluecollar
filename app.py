9a7336d6f59d5db7de3654a2b5260a85fb8ec980
lou <louamaya@me.com>
2021-03-08 15:01:05 -0800

    code cleanup

--- app.py
+++ app.py
@@ -315,9 +315,9 @@ def register(user_type):
         ut = uid.id
         # Verify User Email
         user = Registration(form, ut, filename)
-        if not user.valid_email():
-            form.email.errors.append("Please enter a valid email address!")
-            return render_template("/users/registration.html", form=form, user_type=user_type)
+        # if not user.valid_email():
+        #     form.email.errors.append("Please enter a valid email address!")
+        #     return render_template("/users/registration.html", form=form, user_type=user_type)
 
         # Create user session
         if user_type == 'bluecollar':
